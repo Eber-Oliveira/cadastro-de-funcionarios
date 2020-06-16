d752b1fa45ffbac16ae0a039af65fcbedc6431e8
Eber Oliveira <eber.oliv.costa@gmail.com>
2020-06-16 18:24:35 -0300

    Adiciona Beans

---
+++ src/main/java/com/eberoliveira/cadastrodefuncionarios/business/bean/FuncionariosBean.java
@@ -0,0 +1,23 @@
+package com.eberoliveira.cadastrodefuncionarios.business.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "funcionarios")
+
+public class FuncionariosBean {
+	private List<FuncionarioBean> funcionarios;
+	
+	public FuncionariosBean() {
+		funcionarios = new ArrayList<>();
+		
+	}
+
+	public List<FuncionarioBean> getFuncionarios() {
+		return funcionarios;
+	}
+}
